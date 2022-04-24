aacacf807fb727cf4f586eba66ebe6ca2ca43e9b
mattyicefire <81605759+mattyicefire@users.noreply.github.com>
2022-04-23 20:25:27 -0400

    many different routes, and doing a dumb auth for fun

--- index.js
+++ index.js
@@ -3,19 +3,49 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(morgan('common'));
+
 app.use((req, res, next) => {
-    console.log("This is my middleware");
-    return next();
-    console.log("This is my middleware AGAIN");
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
 })
 
+app.use('/gyms', (req, res, next) => {
+    console.log('GYM ROUTE');
+    next();
+})
+
+const verifyPassword = app.use((req, res, next) => {
+    const { password } = req.query;//never use a query for a password
+    if (password === 'gains') {
+        next();
+    } 
+    res.send('Unauthorized');
+})
+
+// app.use((req, res, next) => {
+//     console.log("This is my middleware");
+//     return next();
+//     console.log("This is my middleware AGAIN");
+// })
+
 app.get('/', (req, res) => {
+    console.log(`REQUEST D8: ${req.requestTime}`);
     res.send('Hello World!');
 });
 
 app.get('/gyms', (req, res) => {
+    console.log(`REQUEST D8: ${req.requestTime}`);
     res.send('Hello Gyms!');
 });
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('Secret!');
+})
+
+app.use((req, res, next) => {
+   res.status(404).send('404 Not Found');
+})
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
